34f20336f19550989988712e6920f62b60aa51e4
Jiangqiu Shen <ivy22233qiu@live.com>
2022-04-19 16:26:42 -0400

    fix test

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod test {
 
     #[test]
     fn test() {
-        let matrix: TriMat<u32> = sprs::io::read_matrix_market("./test.mtx").unwrap();
+        let matrix: TriMat<i32> = sprs::io::read_matrix_market("./test.mtx").unwrap();
         let csc: CsMat<_> = matrix.to_csc();
         println!("{:?}", csc);
     }
