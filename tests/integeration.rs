69a4ad006224eea5b74dede6ed85ae703ce9473e
Jiangqiu Shen <ivy22233qiu@live.com>
2022-04-22 05:53:01 -0400

    update

--- tests/integeration.rs
+++ tests/integeration.rs
@@ -3,7 +3,8 @@ use eyre::Result;
 use log::debug;
 use spmm_pim::result::save_result_list;
 use spmm_pim::run::run_exp;
-use spmm_pim::{result::Results, run_unroll, settings::Settings};
+use spmm_pim::run_1d_c_unroll;
+use spmm_pim::{result::Results, settings::Settings};
 use std::path::Path;
 
 #[test]
@@ -21,7 +22,7 @@ fn test() -> Result<()> {
     let mut err_list = vec![];
     // load config into ConfigFile
     for i in mtxs.iter() {
-        run_unroll!(i;full_result;ok_list;err_list; run_exp; 64,128,256,512,1024,2048);
+        run_1d_c_unroll!(i;full_result;ok_list;err_list; run_exp; 64,128,256,512,1024,2048);
     }
     full_result.save_to_file(Path::new("result_test.json"))?;
     save_result_list(&ok_list, &err_list, Path::new("result_test.json"))?;
