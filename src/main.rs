ac5036c3a850a70fc4764042f97dafe5ec4f71b6
Jiangqiu Shen <ivy22233qiu@live.com>
2022-04-26 13:08:16 -0400

    fix bug when failed run the bench

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use clap::{Command, IntoApp, Parser};
 use clap_complete::Generator;
 use eyre::{Context, Result};
-use log::{debug, info};
+use log::{debug, info, error};
 use spmm_pim::result::save_result_list;
 use spmm_pim::run::run_exp_csr;
 use spmm_pim::utils::init_log;
@@ -41,10 +41,18 @@ fn main() -> Result<()> {
     let mut err_list = vec![];
     // load config into ConfigFile
     for i in mtxs.iter() {
-        let csr = sprs::io::read_matrix_market(i)?.to_csr();
-        run_1d_c_unroll_buf!(i;&csr;&settings.mem_settings;full_result;ok_list;err_list; run_exp_csr; 64,128,256,512,1024,2048);
-        run_2d_unroll_buf!(i;&csr;&settings.mem_settings; full_result;ok_list;err_list; run_exp_csr; (2,32),(4,16),(8,8),(2,64),(4,32),(8,16),(2,128),(4,64),(8,32),(16,16),(2,256),(4,128),(8,64),(16,32),
-        (2,512),(4,256),(8,128),(16,64),(32,32), (2,1024),(4,512),(8,256),(16,128),(32,64));
+        match sprs::io::read_matrix_market(i) {
+            Ok(tri) => {
+                let csr = tri.to_csr();
+                run_1d_c_unroll_buf!(i;&csr;&settings.mem_settings;full_result;ok_list;err_list; run_exp_csr; 64,128,256,512,1024,2048);
+                run_2d_unroll_buf!(i;&csr;&settings.mem_settings; full_result;ok_list;err_list; run_exp_csr; (2,32),(4,16),(8,8),(2,64),(4,32),(8,16),(2,128),(4,64),(8,32),(16,16),(2,256),(4,128),(8,64),(16,32),
+                (2,512),(4,256),(8,128),(16,64),(32,32), (2,1024),(4,512),(8,256),(16,128),(32,64));
+            }
+            Err(e) => {
+                err_list.push(i);
+                error!("{}", e);
+            }
+        }
     }
     let file_name = settings.result_file;
     full_result
